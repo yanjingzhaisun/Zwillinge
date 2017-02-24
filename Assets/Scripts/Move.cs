4c2d4a2310ac6dbf32ccfe2c4818f732dc56594a
Chaojin <jinchaoonly@gmail.com>
2017-02-24 13:13:47 -0600

    Chao changes

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	private new Vector2 PlayerPosition;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPosition = this.transform.position;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			PlayerPosition.y++;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			PlayerPosition.y--;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			PlayerPosition.x--;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			PlayerPosition.x++;
+		}
+
+		this.transform.position = PlayerPosition; 
+		
+	}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	public GameObject scoretext;
+	public new float score;
+	new int count;
+	new float time;
+
+	// Use this for initialization
+	void Start () {
+		time = 0f;
+		score = 0f;
+		count = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (time >= 1f) {
+			score = (score + 1f*count);
+			time = 0f;
+		}
+
+		//scoretext.GetComponent<TextMesh> ().text = "Score:" + score;
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		count = count + 1;
+		Debug.Log (count);
+	}
+
+	void OnCollisionStay2D(Collision2D col){
+
+		time += Time.deltaTime;
+
+
+	}
+
+	void OnCollisionExit2D(Collision2D col){
+		count = count - 1;
+	}
+	
+
+		
+		
+
+}
