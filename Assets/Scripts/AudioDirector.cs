b6d6908f31e91cedaeef9527fc0b7f8568b52af6
Euphonux <zz1322@nyu.edu>
2017-02-24 09:41:59 -0700

    audio director finished.

---
+++ Assets/Scripts/AudioDirector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioDirector : MonoBehaviour {
+	public static AudioDirector instance;
+	public List<AudioMixerSnapshot> snapshots;
+
+	void Awake() {
+		if (instance == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			instance = this;
+		}
+		else if (instance != this)
+			Destroy(gameObject);
+	}
+
+	public void PlaySnapshots(int stages) {
+		stages = Mathf.Clamp(stages, 0, snapshots.Count - 1);
+		snapshots[stages].TransitionTo(0.5f);
+	}
+
+	void Start() {
+		PlaySnapshots(1);
+	}
+
+	void Update() {
+#if UNITY_EDITOR
+		if (Input.GetKey(KeyCode.LeftShift)) {
+			if (Input.GetKeyUp(KeyCode.Alpha0))
+				PlaySnapshots(0);
+			else if (Input.GetKeyUp(KeyCode.Alpha1))
+				PlaySnapshots(1);
+			else if (Input.GetKeyUp(KeyCode.Alpha2))
+				PlaySnapshots(2);
+			else if (Input.GetKeyUp(KeyCode.Alpha3))
+				PlaySnapshots(3);
+			else if (Input.GetKeyUp(KeyCode.Alpha4))
+				PlaySnapshots(4);
+			else if (Input.GetKeyUp(KeyCode.Alpha5))
+				PlaySnapshots(5);
+		}
+		#endif
+	}
+
+}
